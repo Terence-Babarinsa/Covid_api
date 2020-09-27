f6359b4fbd6092fc4b7ae12ec9b6a591606779c2
diogomatos97 <diogopsnskullcrap@gmail.com>
2020-09-27 17:16:32 +0100

    copy

--- Covid_API/Covid_API/CovidService/DataHandling/CovidModel.cs
+++ Covid_API/Covid_API/CovidService/DataHandling/CovidModel.cs
@@ -8,4 +8,25 @@ using Newtonsoft.Json;
 namespace Covid_API
 {
 
+    public class Rootobject
+    {
+        public Class1[] Property1 { get; set; }
+    }
+
+    public class Class1
+    {
+        public string Country { get; set; }
+        public string CountryCode { get; set; }
+        public string Province { get; set; }
+        public string City { get; set; }
+        public string CityCode { get; set; }
+        public string Lat { get; set; }
+        public string Lon { get; set; }
+        public int Confirmed { get; set; }
+        public int Deaths { get; set; }
+        public int Recovered { get; set; }
+        public int Active { get; set; }
+        public DateTime Date { get; set; }
+    }
+
 }
